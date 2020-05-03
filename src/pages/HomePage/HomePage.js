8fcec3a24e30dc9cbc4496a62aa7b2f56f7c4761
NgocAnh <ngoc.anh@jvb-corp.com>
2020-05-03 10:35:26 +0700

    test lazy loading

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,10 +1,19 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 
 class HomePage extends Component {
+    testLazyLoad = () => {
+        import(/* webpackChunkName: "print" */ './print').then(module => {
+            const print = module.default;
+            print();
+        });
+    }
+
+
     render() {
         return (
             <div className="container">
                 <h1>Trang Chủ</h1>
+                <button onClick={this.testLazyLoad}>Test Lazy loading</button>
             </div>
         );
     }
